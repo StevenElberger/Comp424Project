be5a1fd7fb17fec4961c0e79e3ed55b68352c5f9
Steven Elberger <stevenelberger@gmail.com>
2015-04-24 14:51:08 -0700

    Merge Michael's branch

--- private/definitions.php
+++ private/definitions.php
@@ -10,16 +10,11 @@
    // Add definitions that can be useful here
 
    define("DB_SERVER", "localhost");
-   define("DB_USER", "root");
-   define("DB_PASS", "#mws1992");
-   define("DB_NAME", "testDB");
+   define("DB_USER", "424Website");
+   define("DB_PASS", "comp424test");
+   define("DB_NAME", "comp424project");
    define("EMAIL_USERNAME", "healthmateemail@yahoo.com");
    define("EMAIL_PASSWORD", "#490testEmail");
-
-	/*define("DB_SERVER", "localhost");
-	define("DB_USER", "HMTest");
-	define("DB_PASS", "comp490");
-	define("DB_NAME", "testdb");*/
 
 
 ?>
